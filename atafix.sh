201c06779fb4e8fd0bcc31d10b1b20d513722117
Kevin McMurtrie <mcmurtrie@pixelmemory.us>
2025-08-01 16:33:21 -0700

    Update atafix.sh
    Comments

--- atafix.sh
+++ atafix.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+# This goes in /usr/sbin/atafix.sh
+# Disables med_power_with_dipm on ATA devices to fix random errors with frozen commands and DMA corruption
 for f in /sys/class/scsi_host/host*/link_power_management_policy
  do grep -q -F 'med_power_with_dipm' "$f" && echo "Setting max_performance in $f" && (echo 'max_performance' > "$f")
 done
