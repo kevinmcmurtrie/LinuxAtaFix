9e1c8d2658bfa9a51e889c93924d462ae374f8d6
Kevin McMurtrie <mcmurtrie@pixelmemory.us>
2025-08-01 16:38:58 -0700

    Update atafix.sh
    Fix return code

--- atafix.sh
+++ atafix.sh
@@ -5,3 +5,4 @@
 for f in /sys/class/scsi_host/host*/link_power_management_policy
  do grep -q -F 'med_power_with_dipm' "$f" && echo "Setting max_performance in $f" && (echo 'max_performance' > "$f")
 done
+exit 0
